4d427534afc70cb33ec260e0dda113da34925e41
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-12 16:45:35 +0000

    Ajout d'une fonction utilitaire pour donner des temps d'execution

---
+++ MultiThreading1/Utils/ExecutionTime.java
@@ -0,0 +1,27 @@
+package Utils;
+
+public class ExecutionTime {
+    private int speed = 1;
+    private RandomNumberGen gen;
+
+    public ExecutionTime(RandomNumberGen gen, int speed) {
+        this.gen = gen;
+        this.speed = speed;
+    }
+
+    public void speedUp() {
+        speed++;
+    }
+
+    public void speedDown() {
+        speed--;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public double getExecutionTime() {
+        return gen.getRandomNumber(1000)/speed;
+    }
+}
