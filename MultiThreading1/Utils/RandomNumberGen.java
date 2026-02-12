f766bfa55a4cd714db80a21b93caca48783234ef
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-12 16:46:15 +0000

    ajout d'une fonction utilitaire pour generer des nombres aleatoire

---
+++ MultiThreading1/Utils/RandomNumberGen.java
@@ -0,0 +1,18 @@
+package Utils;
+
+import java.util.Random;
+
+public class RandomNumberGen {
+    Random gen;
+    public RandomNumberGen(long seed) {
+        gen = new Random(seed);
+    }
+
+    public int getRandomNumber() {
+        return gen.nextInt();
+    }
+
+    public int getRandomNumber(int max) {
+        return gen.nextInt(max);
+    }
+}
