922c71fa7a8b4667336da066270b6459df7acf98
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-12 13:43:00 +0000

    Creation du client

---
+++ MultiThreading1/assets/Client.java
@@ -0,0 +1,13 @@
+namespace assets;
+
+public class Client {
+    private int queue = 0; //0 = not in a queue ,  >0 = the place in the queue
+
+    public synchronized int getQueue() {
+        return queue;
+    }
+
+    public synchronized void setQueue(int queue) {
+        this.queue = queue;
+    }
+}
