635659661a05f9209d41ae455d6585b951b24833
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-17 18:50:36 +0000

    correction typo

--- MultiThreading1/assets/Client.java
+++ MultiThreading1/assets/Client.java
@@ -29,7 +29,7 @@ public class Client {
                 output = "coffee";
             }
             else {
-                ourput = "tea";
+                output = "tea";
             }
         }
         else {
