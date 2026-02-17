aa84302eeb3ad887190a8635a2e154ec01e851df
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-17 18:52:12 +0000

    Ajout du thres-d sui gere les staff

---
+++ MultiThreading1/Thread/StaffRunnable.java
@@ -0,0 +1,52 @@
+package Thread;
+
+import assets.*;
+import Utils.*;
+import Event.*;
+import java.util.concurrent.BlockingQueue;
+
+public class StaffRunnable  implements Runnable {
+    private Buffet buffet;
+    private Staff staff;
+    private volatile boolean running = true;
+    private ExecutionTime execTime;
+    private RandomNumberGen genAlea;
+    private BlockingQueue<MessageEvent> queue;
+
+    public StaffRunnable(Buffet buffet,Staff staff,ExecutionTime execTime,
+                         RandomNumberGen genAlea,BlockingQueue<MessageEvent> queue) {
+        this.buffet = buffet;
+        this.staff = staff;
+        this.execTime = execTime;
+        this.genAlea = genAlea;
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                String threadName = Thread.currentThread().getName();
+                int time = (int) execTime.getExecutionTime();
+                int nbProduct = genAlea.getRandomNumber(5);
+                if (nbProduct == 0) {
+                    nbProduct = 1;
+                }
+                Thread.sleep(time);
+                synchronized(buffet) {
+                    staff.addToBuffet(buffet,nbProduct);
+                }
+                queue.put(new MessageEvent(Categorie.STAFF,staff.toString(),
+                        threadName,nbProduct) );
+                queue.put( new MessageEvent(Categorie.BUFFET,buffet));
+                queue.put( new MessageEvent(Categorie.KITCHEN,threadName));
+            }
+            catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                running = false;
+            }
+
+        }
+    }
+
+}
