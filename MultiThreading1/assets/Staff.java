da0ac44dc6820a8f5df5fa939c20333c9f60ccf8
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-17 16:48:46 +0000

    AJout d'une methode toString pour obtenir le noms des produits

--- MultiThreading1/assets/Staff.java
+++ MultiThreading1/assets/Staff.java
@@ -6,6 +6,26 @@ public class Staff {
         this.product = product;
     }
 
+    public Product getProduct(){
+        return product;
+    }
+
+    public String toString(){
+        switch(product) {
+            case Product.TEA:
+                return "tea";
+                break;
+            case Product.COFFEE:
+                return "coffee";
+                break;
+            case Product.CAKE:
+                return "cake";
+                break;
+            default:
+                break;
+        }
+    }
+
     public synchronized void addToBuffet(Buffet buffet,int quantity) {
         switch(product) {
             case Product.TEA :
