cdea26282a37827cfadd39d1c369fdf6e0a42354
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-18 12:23:50 +0000

    deplacement de toute la logique dans lee thread

--- MultiThreading1/assets/Client.java
+++ MultiThreading1/assets/Client.java
@@ -1,14 +1,4 @@
 package assets;
 
 public class Client {
-
-    public synchronized int getQueue() {
-        return queue;
-    }
-
-    public synchronized void setQueue(int queue) {
-        this.queue = queue;
-    }
-
-
 }
