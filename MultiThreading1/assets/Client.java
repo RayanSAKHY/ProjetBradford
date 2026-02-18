c28599befa561145cc22496bc2b02970bb306b6b
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-18 10:09:36 +0000

    Ajout de 2 methodes pour choisir l'activite qu'il fait et la  renvoyer

--- MultiThreading1/assets/Client.java
+++ MultiThreading1/assets/Client.java
@@ -1,7 +1,6 @@
 package assets;
 
 public class Client {
-    private int queue = 0; //0 = not in a queue ,  >0 = the place in the queue
 
     public synchronized int getQueue() {
         return queue;
@@ -11,39 +10,5 @@ public class Client {
         this.queue = queue;
     }
 
-    public String entertain(double nbAlea) {
-        String output = "";
-        if (nbAlea > 0.5) {
-            output = "piano";
-        }
-        else {
-            output = "music";
-        }
-        return output;
-    }
 
-    public String consume(double nbAlea) {
-        String output = "";
-        if (nbAlea > 0.5) {
-            if (nbAlea > 0.75) {
-                output = "coffee";
-            }
-            else {
-                output = "tea";
-            }
-        }
-        else {
-            if ( nbAlea > 0.33) {
-                output = "cake";
-            }
-            else if (nbAlea > 0.165) {
-                output = "cake,coffee";
-            }
-            else {
-                output = "cake,tea";
-            }
-        }
-
-        return output;
-    }
 }
