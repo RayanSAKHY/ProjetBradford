28374a81b42e4b9de64ddf35375a95bb814d2af8
rasakhy <rayan.sakhy@etu.isima.fr>
2026-02-18 10:10:48 +0000

    Ajout d'un lock et d'une methode pour verifier la disponibilite du buffet et une autre pour recuperer des produits

--- MultiThreading1/assets/Buffet.java
+++ MultiThreading1/assets/Buffet.java
@@ -1,13 +1,42 @@
 package assets;
+
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Buffet {
     private int tea = 0;
     private int coffee = 0;
     private int cake = 0;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public Buffet(int tea, int coffee, int cake) {
         this.tea = tea;
         this.coffee = coffee;
         this.cake = cake;
+    }
+
+    public synchronized boolean isAvailable(int nbTea,int nbCoffee,int nbCake) {
+        return cake > nbCake || coffee > nbCoffee || tea > nbTea;
+    }
+
+    public boolean tryUse() {
+        return lock.tryLock();
+    }
+
+    public void release() {
+        lock.unlock();
+    }
+
+    public synchronized boolean takeProduct(Product p,int quantity) {
+        switch(p) {
+            case CAKE:
+                if(cake > quantity) { cake-=quantity; return true; } else return false;
+            case COFFEE:
+                if(coffee > quantity) { coffee-=quantity; return true; } else return false;
+            case TEA:
+                if(tea > quantity) { tea-=quantity; return true; } else return false;
+            default:
+                return false;
+        }
     }
 
     public synchronized int getTea() {
