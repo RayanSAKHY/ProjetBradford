aeb6e94266f5681bd0dfa2a7a8c529a2bd8588b7
rasakhy <rayan.sakhy@etu.isima.fr>
2026-03-06 15:24:20 +0000

    retirer les dependances inutiles

--- SoftwareDesignAndTesting1/Software1/src/main/java/Bradford/CWW/App.java
+++ SoftwareDesignAndTesting1/Software1/src/main/java/Bradford/CWW/App.java
@@ -1,11 +1,5 @@
 package Bradford.CWW;
 
-import Bradford.CWW.MFA.*;
-import Bradford.CWW.asssets.User;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Scanner;
-
 /**
  * Hello world!
  */
@@ -14,7 +8,7 @@ public class App {
     public static void main(String[] args) {
         Login login = new Login();
         System.out.println("Hello World!");
-        System.out.println(login.login("test","sdffsyrkkxhj"));
+        System.out.println(login.login("test","azerty"));
 //        System.out.println(login("test","",users));
 //        System.out.println(login("tEst","",users));
 //
