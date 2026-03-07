986e35c75f301d197b08692bc9173456e208b158
rasakhy <rayan.sakhy@etu.isima.fr>
2026-03-07 19:40:02 +0000

    retirer l'id

--- SoftwareDesignAndTesting1/Software1/src/main/java/Bradford/CWW/asssets/User.java
+++ SoftwareDesignAndTesting1/Software1/src/main/java/Bradford/CWW/asssets/User.java
@@ -1,7 +1,6 @@
 package Bradford.CWW.asssets;
 
 public class User{
-    private Long id;
 
     private String username;
 
@@ -21,14 +20,6 @@ public class User{
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public User() {
