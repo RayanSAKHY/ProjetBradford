c315cd721d4a09804f6e12a811e41d0928947e72
rasakhy <rayan.sakhy@etu.isima.fr>
2026-03-10 16:09:05 +0000

    finition de l'app pour gerer les qrcode

--- SoftwareDesignAndTesting1/Software1/src/main/java/Bradford/CWW/MFA/AuthentificatorApp.java
+++ SoftwareDesignAndTesting1/Software1/src/main/java/Bradford/CWW/MFA/AuthentificatorApp.java
@@ -17,99 +17,100 @@ import java.io.IOException;
 import java.util.Scanner;
 import javax.swing.*;
 
-public class AuthentificatorApp implements IMFAStrategy {
-    private final TimeProvider timeProvider;
-    private final CodeGenerator codeGenerator;
-    private final Scanner scanner;
-    private final QrGenerator qrGenerator;
-    private final SecretGenerator secretGenerator;
+public class AuthentificatorApp{
+    protected TimeProvider timeProvider;
+    protected CodeGenerator codeGenerator;
+    protected QrGenerator qrGenerator;
+    protected SecretGenerator secretGenerator;
+    private GUIQrCode guiQrCode;
+    private String Path;
+    protected boolean testMode;
+
 
     public AuthentificatorApp() {
-        this(new SystemTimeProvider(), new DefaultCodeGenerator(),new Scanner(System.in),new ZxingPngQrGenerator(),new DefaultSecretGenerator());
+        this(new SystemTimeProvider(), new DefaultCodeGenerator(),new ZxingPngQrGenerator(),new DefaultSecretGenerator());
     }
 
-    public AuthentificatorApp(TimeProvider timeProvider,CodeGenerator codeGenerator,Scanner scanner, QrGenerator qrGenerator,SecretGenerator secretGenerator) {
+    public void setTestMode(boolean value) {
+        this.testMode = value;
+    }
+
+    public AuthentificatorApp(TimeProvider timeProvider,CodeGenerator codeGenerator, QrGenerator qrGenerator,SecretGenerator secretGenerator) {
         this.timeProvider = timeProvider;
         this.codeGenerator = codeGenerator;
         this.qrGenerator = qrGenerator;
         this.secretGenerator = secretGenerator;
-        this.scanner = scanner;
     }
 
-    public AuthentificatorApp(TimeProvider timeProvider,CodeGenerator codeGenerator,Scanner scanner) {
-        this(timeProvider,codeGenerator,scanner,new ZxingPngQrGenerator(),new DefaultSecretGenerator());
+    public AuthentificatorApp(TimeProvider timeProvider,CodeGenerator codeGenerator) {
+        this(timeProvider,codeGenerator,new ZxingPngQrGenerator(),new DefaultSecretGenerator());
     }
 
-    public AuthentificatorApp(Scanner scanner) {
-        this(new SystemTimeProvider(),new DefaultCodeGenerator(),scanner);
-    }
-    public AuthentificatorApp(TimeProvider timeProvider, CodeGenerator codeGenerator) {
-        this(timeProvider,codeGenerator,new Scanner(System.in),new ZxingPngQrGenerator(),new DefaultSecretGenerator());
-    }
+    public void generateQrCode(String secret,String label,String path) throws QrGenerationException,IOException {
+        Path = path;
 
-    @Override
-    public boolean TwoStepVerif() {
-        String secret = secretGenerator.generate();
         // secret = "BP26TDZUZ5SVPZJRIHCAUVREO5EWMHHV"
-        try {
-            byte[] imageData = generateQRCodeData(secret);
-            generateImage(imageData);
-            SwingUtilities.invokeLater(() -> {
-                        GUIQrCode guiQrCode= new GUIQrCode("src/main/java/Bradford/CWW/MFA/QRCode.png");
-                        guiQrCode.printQrCode();
+        byte [] imageData= generateQRCodeData(secret,label);
 
+        generateImage(imageData,path);
+        if (!testMode) {
+            SwingUtilities.invokeLater(() -> {
+                        this.guiQrCode= new GUIQrCode(Path);
+                        this.guiQrCode.printQrCode();
                     }
             );
         }
-        catch (QrGenerationException | IOException ex) {
-            ex.printStackTrace();
-            System.err.println(ex.getMessage());
-            return false;
-        }
-
-        System.out.println("Use the QRCode in your authentificator app and type the code you obtain");
-        String code = scanner.nextLine();
-        return verifyCode(secret, code);
     }
 
-    private byte[] generateQRCodeData(String secret) throws dev.samstevens.totp.exceptions.QrGenerationException {
+    private byte[] generateQRCodeData(String secret,String label) throws QrGenerationException {
         QrData data = new QrData.Builder()
-                .label("example@example.com")
+                .label(label)
                 .secret(secret)
                 .issuer("AppName")
                 .algorithm(HashingAlgorithm.SHA1) // More on this below
                 .digits(6)
-                .period(30)
+                .period(60)
                 .build();
         return qrGenerator.generate(data);
 
     }
 
-    private void generateImage(byte[] imageData) throws IOException {
+    private void generateImage(byte[] imageData,String path) throws IOException {
         // Source - https://stackoverflow.com/q/15477152
         // Posted by Arvind Sridharan, modified by community. See post 'Timeline' for change history
         // Retrieved 2026-03-06, License - CC BY-SA 3.0
-            if (imageData == null) {
-                throw new IOException("Image data is null");
-            }
-            if (imageData.length == 0) {
-                throw new IOException("Image data is empty");
-            }
-            FileOutputStream fileOutputStream = new FileOutputStream("src/main/java/Bradford/CWW/MFA/QRCode.png");
-            fileOutputStream.write(imageData);
-            fileOutputStream.close();
-            System.out.println("QR Code generated: QRCode.png");
+        if (imageData == null) {
+            throw new IOException("Image data is null");
+        }
+        if (imageData.length == 0) {
+            throw new IOException("Image data is empty");
+        }
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        fileOutputStream.write(imageData);
+        fileOutputStream.close();
+        System.out.println("QR Code generated ");
     }
 
     public boolean verifyCode(String secret, String code) {
         DefaultCodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
 
+        end();
+
+        verifier.setTimePeriod(60);
         verifier.setAllowedTimePeriodDiscrepancy(1);
 
         // secret = the shared secret for the user
         // code = the code submitted by the user
         code = code.trim(); //to remove the space
         return verifier.isValidCode(secret, code);
+    }
+
+    public void end() {
+        if (guiQrCode != null && !testMode){
+            SwingUtilities.invokeLater(() -> {
+                this.guiQrCode.end();
+            });
+        }
     }
 }
 
