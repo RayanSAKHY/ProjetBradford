34d20094fbc1990f0628b9575b17c6e1d2ec1a79
rasakhy <rayan.sakhy@etu.isima.fr>
2026-04-01 18:46:31 +0100

    ajout de l'option pour le GUI

--- SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/App.java
+++ SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/App.java
@@ -1,9 +1,16 @@
 package Bradford.CWW;
 
-public class App {
+import Bradford.CWW.GUI.MainGUI;
 
-    public static void main(String[] args) {
-        UserInterface app = new UserInterface();
-        app.UseApp();
+public class App{
+
+    public static void main(String[] args){
+        if (args.length > 0 && args[0].equals("GUI")) {
+            javafx.application.Application.launch(MainGUI.class);
+        }
+        else {
+            UserInterface app = new UserInterface();
+            app.UseApp();
+        }
     }
 }
