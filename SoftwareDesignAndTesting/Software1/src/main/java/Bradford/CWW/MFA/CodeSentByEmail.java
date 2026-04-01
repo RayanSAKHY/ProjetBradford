d45e715dbd1dd96093d1bbc97189821f8504ea20
rasakhy <rayan.sakhy@etu.isima.fr>
2026-04-01 18:46:47 +0100

    utilisation de l'input abstrait

--- SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/MFA/CodeSentByEmail.java
+++ SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/MFA/CodeSentByEmail.java
@@ -1,22 +1,23 @@
 package Bradford.CWW.MFA;
 
+import Bradford.CWW.Input.UserInput;
+
 import java.util.Scanner;
 //Source : https://stackoverflow.com/questions/884943/how-do-i-send-an-e-mail-in-java
 
 
 
 public class CodeSentByEmail implements IMFAStrategy {
-    private final Scanner scanner;
+    private final UserInput userInput;
 
-    public CodeSentByEmail(Scanner scanner) {
-        this.scanner = scanner;
+    public CodeSentByEmail(UserInput userInput) {
+        this.userInput = userInput;
     }
 
     @Override
     public boolean TwoStepVerif() {
-        System.out.println("Please enter your email to receive a code by email: ");
-        String email = scanner.nextLine();
-        System.out.println("sent a code to "+email);
+        String email = userInput.askInput("Please enter your email to receive a code by email: ");
+        userInput.showMessage("sent a code to "+email) ;
         return true;
     }
 }
