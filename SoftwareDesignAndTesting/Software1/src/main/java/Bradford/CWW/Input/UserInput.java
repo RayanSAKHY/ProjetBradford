6d4857770f85796a9597554a472c510abdbd8bdd
rasakhy <rayan.sakhy@etu.isima.fr>
2026-04-03 17:17:45 +0100

    ajout d'une version async pour chaque fonction

--- SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/Input/UserInput.java
+++ SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/Input/UserInput.java
@@ -1,7 +1,10 @@
 package Bradford.CWW.Input;
 
+import java.util.function.Consumer;
+
 public interface UserInput {
     String askInput(String message);
+    void askInputAsync(String message, Consumer<String> queue);
     void showMessage(String message);
     void close();
 }
--- SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/MFA/IMFAStrategy.java
+++ SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/MFA/IMFAStrategy.java
@@ -1,5 +1,8 @@
 package Bradford.CWW.MFA;
 
+import java.util.function.Consumer;
+
 public interface IMFAStrategy {
     public boolean TwoStepVerif();
+    public void TwoStepVerifAsync(Consumer<String> queue);
 }
