8adb235d6e0ea754afcca9c758e9fccaa0135610
rasakhy <rayan.sakhy@etu.isima.fr>
2026-04-03 17:32:59 +0100

    enlever les public de l'interface

--- SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/MFA/IMFAStrategy.java
+++ SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/MFA/IMFAStrategy.java
@@ -3,6 +3,6 @@ package Bradford.CWW.MFA;
 import java.util.function.Consumer;
 
 public interface IMFAStrategy {
-    public boolean TwoStepVerif();
-    public void TwoStepVerifAsync(Consumer<String> queue);
+    boolean TwoStepVerif();
+    void TwoStepVerifAsync(Consumer<String> queue);
 }
