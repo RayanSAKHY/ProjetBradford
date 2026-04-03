c6665302ccf864b3e51bcd586f9ca342a8c98430
rasakhy <rayan.sakhy@etu.isima.fr>
2026-04-03 17:34:43 +0100

    application de userInput a javafx

--- SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/Input/JavaFXInput.java
+++ SoftwareDesignAndTesting/Software1/src/main/java/Bradford/CWW/Input/JavaFXInput.java
@@ -1,25 +1,43 @@
 package Bradford.CWW.Input;
 
-import java.awt.*;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.util.function.Consumer;
 
 public class JavaFXInput implements UserInput {
     private TextArea textArea;
     private TextField textField;
+    private Button button;
 
-    public JavaFXInput(TextArea textArea, TextField textField) {
+    public JavaFXInput(TextArea textArea, TextField textField, Button button) {
         this.textArea = textArea;
         this.textField = textField;
+        this.button = button;
     }
 
     @Override
     public String askInput(String message) {
-        textArea.append(message+"\n");
-        return textField.getText();
+        return null;
+    }
+
+    @Override
+    public void askInputAsync(String message, Consumer<String> queue) {
+        textArea.setText(message);
+
+        button.setOnAction(event -> {
+            String input = textField.getText();
+            textField.clear();
+            queue.accept(input);
+        });
+
     }
 
     @Override
     public void showMessage(String message) {
-        textArea.append(message+"\n");
+        textArea.setText(message+"\n");
     }
 
     @Override
